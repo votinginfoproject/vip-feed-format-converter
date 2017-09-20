d68e4893e20e57bb131fd46129a3cc0c6b18243d
Wes Morgan <wesmorgan@icloud.com>
2017-09-20 11:59:33 -0600

    Replace template test w/ some real ones

--- test/vip_feed_format_converter/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns vip-feed-format-converter.core-test
-  (:require [clojure.test :refer :all]
-            [vip-feed-format-converter.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
---
+++ test/vip_feed_format_converter/util_test.clj
@@ -0,0 +1,24 @@
+(ns vip-feed-format-converter.util-test
+  (:require [clojure.test :refer :all]
+            [vip-feed-format-converter.util :refer :all]))
+
+(deftest assoc-chars-test
+  (testing "assoc's :str value from event arg into ctx arg at [:tmp top-level-key key]"
+    (is (= {:tmp {:tlk {:key "the thing with the deal"}}}
+           (assoc-chars :tlk {} {:str "the thing with the deal"} :key))))
+  (testing "trims the :str value from event arg"
+    (is (= {:tmp {:tlk {:key "  the thing with the deal "}}})
+       (assoc-chars :tlk {} {:str "the thing with the deal"} :key))))
+
+(deftest assoc-intl-text-test
+  (testing "assoc-char's when language matches"
+    (is (= {:tmp {:tlk {:key "moar things"}}}
+           (assoc-intl-text :tlk "en" {} {:str "moar things"
+                                          :prior {:attrs {:language "en"}}}
+                            :key))))
+  (testing "returns unmodified ctx when language doesn't match"
+    (is (= {:other :thingy})
+        (assoc-intl-text :tlk "en" {:other :thingy}
+                         {:str "más cosas"
+                          :prior {:attrs {:language "es"}}}
+                         :key))))
