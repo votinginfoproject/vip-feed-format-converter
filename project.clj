db5b7b87db92ea8fe585a3ca6212024908439df1
Wes Morgan <wesmorgan@icloud.com>
2017-09-21 06:38:26 -0600

    Switch to BSD 3-clause license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject vip-feed-format-converter "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "BSD 3-Clause License"
+            :url "https://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.2.0-alpha2"]
                  [org.clojure/data.csv "0.1.4"]]
