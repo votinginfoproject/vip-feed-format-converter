034e4bde28280fd32f6ccc6d535aa4c6802f908e
Troy Hill <troy@democracy.works>
2017-09-26 11:04:24 -0600

    add setup-writer function
    Will setup a thread to write to a csv file pulling
    data in a streamed manner from the channel.
    
    Will wait for the channel to close before exiting,
    and upon exiting, appends back a channel with
    a dummy value (so the main processing can wait
    for writing to be complete before exiting)

--- src/vip_feed_format_converter/csv.clj
+++ src/vip_feed_format_converter/csv.clj
@@ -1,6 +1,27 @@
 (ns vip-feed-format-converter.csv
   (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.core.async :as async]))
+
+(defn setup-writer
+  "Sets up to write to a CSV based on pulling values from the
+   supplied async channel."
+  [ctx type headers channel]
+  (update-in ctx [:write-channels] conj
+             (async/thread
+               (println "Start writing stream for" (name type))
+               (with-open [csv-file (io/writer (str (:out-dir ctx)
+                                                    "/" (name type) ".csv"))]
+                 ;; write the file header
+                 (csv/write-csv csv-file [(map name headers)])
+                 ;; write data as it streams in
+                 (loop [data (async/<!! channel)]
+                   (when data
+                     (csv/write-csv csv-file [(map #(get data %) headers)])
+                     (recur (async/<!! channel))))
+                 (println "Stop writing stream for" (name type)))
+               ;; return something on the thread channel
+               type)))
 
 (defn write-files [ctx]
   (doseq [[file {:keys [headers data]}] (:csv-data ctx)]
