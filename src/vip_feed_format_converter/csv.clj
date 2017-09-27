b758ba346df03d577117afad891976b9534b18d8
Troy Hill <troy@democracy.works>
2017-09-27 08:24:24 -0600

    s/update-in/update

--- src/vip_feed_format_converter/csv.clj
+++ src/vip_feed_format_converter/csv.clj
@@ -7,21 +7,21 @@
   "Sets up to write to a CSV based on pulling values from the
    supplied async channel."
   [ctx type headers channel]
-  (update-in ctx [:write-channels] conj
-             (async/thread
-               (println "Start writing stream for" (name type))
-               (with-open [csv-file (io/writer (str (:out-dir ctx)
-                                                    "/" (name type) ".csv"))]
-                 ;; write the file header
-                 (csv/write-csv csv-file [(map name headers)])
-                 ;; write data as it streams in
-                 (loop [data (async/<!! channel)]
-                   (when data
-                     (csv/write-csv csv-file [(map #(get data %) headers)])
-                     (recur (async/<!! channel))))
-                 (println "Stop writing stream for" (name type)))
-               ;; return something on the thread channel
-               type)))
+  (update ctx :write-channels conj
+          (async/thread
+            (println "Start writing stream for" (name type))
+            (with-open [csv-file (io/writer (str (:out-dir ctx)
+                                                 "/" (name type) ".csv"))]
+              ;; write the file header
+              (csv/write-csv csv-file [(map name headers)])
+              ;; write data as it streams in
+              (loop [data (async/<!! channel)]
+                (when data
+                  (csv/write-csv csv-file [(map #(get data %) headers)])
+                  (recur (async/<!! channel))))
+              (println "Stop writing stream for" (name type)))
+            ;; return something on the thread channel
+            type)))
 
 (defn write-files [ctx]
   (println "Writing cached files")
